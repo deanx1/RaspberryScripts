2d078686ef64074f35ddd6d4c7256a76532b1cad
Dean van Doorn <dean@code-orange.nl>
2019-12-01 01:05:23 +0100

    Test if other sensor works 2

--- sensorScript.py
+++ sensorScript.py
@@ -11,6 +11,7 @@ data['sensor'] = []
 temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
 
 # humidity = round(humidity, 2)
+print (temperature)
 temperature = round(temperature, 2)
 
 if humidity is not None and temperature is not None:
