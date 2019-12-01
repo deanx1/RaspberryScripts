a42fa5975321d34f971c628d22deae66ef394817
Dean van Doorn <dean@code-orange.nl>
2019-12-01 16:50:13 +0100

    refactor

--- DS18SensorScript.py
+++ DS18SensorScript.py
@@ -10,8 +10,6 @@ import json
 data = {}
 data['sensor'] = [] 
 
-
-
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
@@ -33,11 +31,6 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
         return temp_c
-        # return temp_c, temp_f
-
-# while True:
-#     print(read_temp())
-#     time.sleep(1)
 
 temperature = read_temp()
 temperature = round(temperature, 2)
@@ -45,12 +38,10 @@ temperature = round(temperature, 2)
 if temperature is not None:
 
   print 'Temperatuur: {0:0.1f}*C'.format(temperature)
-#   print 'Luchtvochtigheid: {0:0.1f}%'.format(humidity)
   print datetime.now()
   
   data['sensor'].append({
   'temperature': temperature,
-#   'humidity': humidity,
   'datetime': str(datetime.now())
 })
 
